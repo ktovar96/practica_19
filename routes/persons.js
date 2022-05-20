4e33cf3d85e52621dba40bbd7aaf154bc2c07470
unknown <ktovar@ucol.mx>
2022-05-19 22:43:26 -0500

    practicca 19

---
+++ routes/persons.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const mongooose = require('../node_modules/mongoose');
+let Person = require('../models/person');
+
+
+router.get('/persons', function(req, res, next) {
+    Person.find(function (err, persons) {
+        if (err) return next(err);
+        res.json(persons);
+    });
+});
+
+module.exports = router;
